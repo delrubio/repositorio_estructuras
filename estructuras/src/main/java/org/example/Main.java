f8d071165ff4041436f3a51ccbda22cf832d801c
A606\1DAW <paula.varmir@gmail.com>
2025-03-11 17:27:52 +0100

    ejs simples

--- estructuras/src/main/java/org/example/Main.java
+++ estructuras/src/main/java/org/example/Main.java
@@ -1,9 +1,12 @@
 package org.example;
 
+import java.util.Collection;
+import java.util.Stack;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        
+        ejercicios_simples.ejercicio3();
     }
 }
---
+++ estructuras/src/main/java/org/example/ejercicios_simples.java
@@ -0,0 +1,68 @@
+package org.example;
+
+import java.util.Scanner;
+import java.util.Stack;
+
+public class ejercicios_simples {
+
+    static Scanner teclado = new Scanner(System.in);
+
+    public static void ejercicio1(){
+        Stack<Character> letras = new Stack<>();
+
+        letras.push('A');
+        letras.push('B');
+        letras.push('C');
+        letras.push('D');
+        letras.push('E');
+
+        if (letras.isEmpty()){
+            System.out.println("VACÍA");
+        }else{
+            System.out.println("Hay " + letras.size() + " elementos");
+        }
+
+        letras.peek();
+
+        while (!letras.isEmpty()){
+            System.out.println(letras.pop());
+        }
+    }
+
+    public static void ejercicio2(){
+
+        Stack<Integer> inverso = new Stack<>();
+
+        for (int i = 0; i < 3; i++) {
+            System.out.println("Dime un numero: ");
+            inverso.push(teclado.nextInt());
+        }
+
+        System.out.println("NUMERO INVERTIDO: ");
+
+        while(!inverso.isEmpty()){
+            System.out.println(inverso.pop());
+        }
+    }
+
+    public static void ejercicio3(){
+        System.out.print("Expresión: ");
+        String expresion = teclado.next();
+
+        esBalanceado(expresion);
+
+    }
+
+    public static void esBalanceado(String expre){
+
+        Stack<Character> cadena = new Stack<>();
+
+        for (int i = 0; i < expre.length(); i++) {
+            if (")".equals(expre.charAt(i)) && cadena.contains('(')){
+                cadena.pop();
+            } else if ("(".equals(expre.charAt(i))) {
+                cadena.push('(');
+            }
+        }
+    }
+}
