05da2e83394100ba2e240b4c1bd3e1573ca7551d
David Andres <dandreswebs@gmail.com>
2025-05-02 18:41:23 +0200

    esta esto muy bien ya

--- bbdd_relacional/src/main/java/org/example/Main.java
+++ bbdd_relacional/src/main/java/org/example/Main.java
@@ -1,13 +1,15 @@
 package org.example;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import javax.swing.plaf.nimbus.State;
+import java.sql.*;
+import java.time.LocalDate;
 
 public class Main {
     public static void main(String[] args) {
 
         Connection bd = conectar();
+        consultar(bd);
+        modificar(bd);
         desconectar(bd);
 
     }
@@ -39,6 +41,80 @@ public class Main {
             System.out.println(e.getMessage());
             throw new RuntimeException(e);
         }
+    }
+
+    public static void consultar(Connection conexion){
+
+        String query = "SELECT * FROM estudiante";
+
+        Statement stmt;
+        ResultSet resultado;
+
+        try {
+            stmt = conexion.createStatement();
+            resultado = stmt.executeQuery(query);
+
+            while (resultado.next()){
+                int nia = resultado.getInt("nia");
+                String nombre = resultado.getString("nombre");
+                LocalDate date = resultado.getDate("fecha_nacimiento").toLocalDate();
+
+                System.out.println("Nombre - " + nombre + ", nia " + nia + ". Fecha de Nacimiento - " + date);
+            }
+
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    public static void insertar(Connection conexion){
+
+        String query = "INSERT INTO estudiante (nia, nombre, fecha_nacimiento) VALUES ('10207853', 'Roberto', '2000-10-05')";
+
+        Statement stmt;
+
+        try {
+            stmt = conexion.createStatement();
+            stmt.executeUpdate(query);
+            System.out.println("--- FILA INSERTADA ---");
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+            throw new RuntimeException(e);
+        }
+    }
+
+    public  static void modificar(Connection conexion){
+        String query = "UPDATE estudiante SET nombre = 'Pocholo' WHERE nia = '10805613'";
+
+        Statement stmt;
+
+        try {
+            stmt = conexion.createStatement();
+            stmt.executeUpdate(query);
+            System.out.println("--- FILA ACTUALIZADA ---");
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void borrar(Connection conexion){
+
+        String query = "DELETE FROM estudiantes WHERE nombre = 'Roberto'";
+
+        Statement stmt;
+
+        try {
+            stmt = conexion.createStatement();
+            stmt.executeUpdate(query);
+            System.out.println("--- FILA BORRADA ---");
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+            throw new RuntimeException(e);
+        }
+
     }
 
 }
